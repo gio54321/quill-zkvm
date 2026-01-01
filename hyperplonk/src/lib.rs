7b147295081c4ee01f251850df794bd920ff0640
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2026-01-01 17:39:01 +0100

    add fast eq poly eval

--- hyperplonk/src/lib.rs
+++ hyperplonk/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod piops;
+pub mod utils;
---
+++ hyperplonk/src/utils/fast_eq_eval.rs
@@ -0,0 +1,65 @@
+use ark_ff::fields::PrimeField;
+
+/// Evaluate eq(x, r) = prod_{i=1}^n (x_i * r_i + (1 - x_i) * (1 - r_i))
+/// for every point in {0,1}^n
+/// This function takes linear O(2^n) time
+pub fn fast_eq_eval<F: PrimeField>(
+    n : usize,
+    point: &[F],
+) -> Vec<F> {
+    assert_eq!(point.len(), n);
+
+    // at each iteration, we have a vector of size 2^i, containing
+    // the evaluations of eq on the first i variables
+    let mut evals : Vec<F> = vec![F::one()];
+    for i in (0..n).rev() {
+        let r_i = point[i];
+        let one_minus_r_i = F::one() - r_i;
+
+        let mut new_evals : Vec<F> = Vec::with_capacity(evals.len() * 2);
+        for &eval in &evals {
+            // eq(0, x_i+1,...,x_n) = eq(x_1,...,x_n) * (1 - r_{i+1})
+            let eval_0 = eval * one_minus_r_i;
+            new_evals.push(eval_0);
+
+            // eq(1, x_i+1,...,x_n) = eq(x_1,...,x_n) * r_{i+1}
+            let eval_1 = eval * r_i;
+            new_evals.push(eval_1);
+        }
+        evals = new_evals;
+    }
+
+    assert_eq!(evals.len(), 1 << n);
+    evals
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fr;
+    use ark_std::test_rng;
+    use ark_ff::UniformRand;
+    use ark_std::{Zero, One};
+
+    #[test]
+    fn test_fast_eq_eval() {
+        let n = 5;
+        let mut rng = test_rng();
+        let point = (0..n).map(|_| Fr::rand(&mut rng)).collect::<Vec<Fr>>();
+        let evals = fast_eq_eval(n, &point);
+
+        // verify correctness naively
+        for i in 0..(1 << n) {
+            let mut expected_eval = Fr::one();
+            for j in 0..n {
+                let x_j = if (i >> j) & 1 == 1 { Fr::one() } else { Fr::zero() };
+                let r_j = point[j];
+                let term = x_j * r_j + (Fr::one() - x_j) * (Fr::one() - r_j);
+                expected_eval *= term;
+            }
+            assert_eq!(evals[i], expected_eval);
+        }
+    }
+}
