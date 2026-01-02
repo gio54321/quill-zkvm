09c7e4892ad83e3afdec1402adceab87bca35b1f
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2026-01-02 01:32:15 +0100

    add zero check piop

---
+++ hyperplonk/src/piops/zerocheck.rs
@@ -0,0 +1,172 @@
+use ark_ff::PrimeField;
+use ark_std::Zero;
+use quill_transcript::transcript::Transcript;
+use crate::piops::EvaluationClaim;
+use crate::piops::sumcheck::SumcheckProof;
+use crate::utils::virtual_polynomial::VirtualPolynomial;
+use crate::utils::eq_eval::{self, eq_eval, fast_eq_eval_hypercube};
+
+
+pub struct ZeroCheckProof<F: PrimeField> {
+    pub num_vars: usize,
+    pub sumcheck_proof: SumcheckProof<F>,
+}
+
+impl<F: PrimeField> ZeroCheckProof<F> {
+    pub fn prove(h: &VirtualPolynomial<F>, transcript: &mut Transcript) -> Self {
+        let num_vars = h.num_vars;
+        let random_point = (0..num_vars).map(|_| transcript.draw_field_element::<F>()).collect::<Vec<F>>();
+
+        let mut h = h.clone();
+
+        // get evaluations of eq(x, random_point) over {0,1}^n
+        let eq_evals = fast_eq_eval_hypercube(num_vars, random_point.as_slice());
+
+        let eq_poly_index = h.allocate_input_mle(&eq_evals);
+        h.mul_mle(eq_poly_index);
+
+        let sumcheck_proof = SumcheckProof::prove(
+            num_vars,
+            &h,
+            F::zero(),
+            transcript,
+        );
+
+        Self {
+            num_vars,
+            sumcheck_proof
+        }
+    }
+
+    pub fn verify(&self, transcript: &mut Transcript) -> Result<EvaluationClaim<F>, String> {
+        let num_vars = self.num_vars;
+        let random_point = (0..num_vars).map(|_| transcript.draw_field_element::<F>()).collect::<Vec<F>>();
+
+        if self.sumcheck_proof.claimed_sum != F::zero() {
+            return Err("Sumcheck claimed sum is not zero".to_string());
+        }
+
+        if self.sumcheck_proof.num_vars != num_vars {
+            return Err("Sumcheck proof num_vars does not match zerocheck num_vars".to_string());
+        }
+
+        let sumcheck_eval_claim = self.sumcheck_proof.verify(
+            transcript,
+        )?;
+
+        // compute the zerocheck evaluation claim from the sumcheck evaluation claim
+        let eq_eval = eq_eval(
+            &random_point,
+            &sumcheck_eval_claim.point,
+        );
+
+        Ok(EvaluationClaim {
+            point: sumcheck_eval_claim.point,
+            // sumcheck_claimed_evaluation = h(r) * eq_eval
+            evaluation: sumcheck_eval_claim.evaluation / eq_eval,
+        })
+    }
+}
+
+
+mod tests {
+    use crate::utils::virtual_polynomial::VirtualPolyExpr;
+
+    use super::*;
+    use ark_bn254::Fr;
+    use ark_std::One;
+    use ark_poly::{DenseMultilinearExtension, Polynomial};
+
+    #[test]
+    fn test_zerocheck_proof() {
+        let num_vars = 3;
+
+
+        let g1 = DenseMultilinearExtension::from_evaluations_vec(
+            num_vars,
+            vec![
+                Fr::zero(), Fr::one(), Fr::from(2u64), Fr::from(3u64),
+                Fr::from(4u64), Fr::from(5u64), Fr::from(6u64), Fr::from(7u64),
+            ],
+        );
+
+        let g2 = DenseMultilinearExtension::from_evaluations_vec(
+            num_vars,
+            vec![
+                Fr::zero(), Fr::one(), Fr::from(4u64), Fr::from(9u64),
+                Fr::from(16u64), Fr::from(25u64), Fr::from(36u64), Fr::from(49u64),
+            ],
+        );
+        
+
+        // create a virtual polynomial h(g1, g2) = g1 * g1 - g2
+        // we want to prove that h sums to zero over {0,1}^n
+        let (mut virtual_poly, g1_ref) = VirtualPolynomial::from_poly_evals(num_vars, &g1.evaluations);
+        let g2_ref = virtual_poly.allocate_input_mle(&g2.evaluations);
+        virtual_poly.mul_mle(g1_ref);
+        virtual_poly.sub_mle(g2_ref);
+
+        let proof = ZeroCheckProof::prove(
+            &virtual_poly,
+            &mut Transcript::new(b"zerocheck_test"),
+        );
+
+        let evaluation_claim = proof.verify(
+            &mut Transcript::new(b"zerocheck_test"),
+        ).unwrap();
+
+        let point = &evaluation_claim.point;
+
+        // check manually the evaluation claim
+        let g1_at_r : Fr = g1.evaluate(&point);
+        let g2_at_r : Fr = g2.evaluate(&point);
+        let h_at_r = virtual_poly.evaluate_point(&vec![g1_at_r, g2_at_r]);
+        
+        assert_eq!(evaluation_claim.evaluation, h_at_r, "Evaluation claim should match g1 at the evaluation point");
+    }
+
+
+      #[test]
+    fn test_zerocheck_proof_not_zero() {
+        let num_vars = 3;
+
+
+        let g1 = DenseMultilinearExtension::from_evaluations_vec(
+            num_vars,
+            vec![
+                Fr::zero(), Fr::one(), Fr::from(2u64), Fr::from(3u64),
+                Fr::from(4u64), Fr::from(5u64), Fr::from(6u64), Fr::from(7u64),
+            ],
+        );
+
+        let g2 = DenseMultilinearExtension::from_evaluations_vec(
+            num_vars,
+            vec![
+                Fr::zero(), Fr::one(), Fr::from(4u64), Fr::from(9u64),
+                Fr::from(16u64), Fr::from(25u64), Fr::from(36u64), Fr::from(50u64), // error in the last one
+            ],
+        );
+        
+
+        // create a virtual polynomial h(g1, g2) = g1 * g1 - g2
+        // we want to prove that h sums to zero over {0,1}^n
+        let (mut virtual_poly, g1_ref) = VirtualPolynomial::from_poly_evals(num_vars, &g1.evaluations);
+        let g2_ref = virtual_poly.allocate_input_mle(&g2.evaluations);
+        virtual_poly.mul_mle(g1_ref);
+        virtual_poly.sub_mle(g2_ref);
+
+        let proof = ZeroCheckProof::prove(
+            &virtual_poly,
+            &mut Transcript::new(b"zerocheck_test"),
+        );
+
+        let res = proof.verify(
+            &mut Transcript::new(b"zerocheck_test"),
+        );
+
+        match res {
+            Err(_) => {}, // expected
+            Ok(_) => panic!("Zerocheck proof should not verify for non-zero polynomial"),
+        }
+    }
+}
